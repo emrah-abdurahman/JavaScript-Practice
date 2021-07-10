4d5b169f550e487644d0b920d82694883817c66a
Emrah Abdurahman <Emrah.Abdurahman@hotmail.com>
2021-07-10 19:37:59 +0100

    continue practicing

--- jQuery/Practice-Folder-001/javascript/practice.js
+++ jQuery/Practice-Folder-001/javascript/practice.js
@@ -1,10 +1,30 @@
-$(":header").addClass("headline");
-
 $(function () {
-  $("ul").before("<p class='notice'>Just updated</p>");
-  $("li:contains('pine')").text("almonds");
-});
+  $("li").on("click", function (e) {
+    $("li span").remove();
+    let date = new Date();
+    date.setTime(e.timeStamp);
+    let clicked = date.toDateString();
+    $(this).append("<span class='date'>" + clicked + " " + e.type + "</span");
+  });
 
-$("li").on("click", function () {
-  $(this).remove();
+  // let ids = "";
+  // let $listItems = $("li");
+  // $listItems.on("mouseover click", function () {
+  //   ids = this.id;
+  //   $listItems.children("span").remove();
+  //   $(this).append("<span class='priority'>" + ids + "</span>");
+  // });
+  // $listItems.on("mouseout", function () {
+  //   $(this).children("span").remove();
+  // });
+  // $(":header").addClass("headline");
+  // $("ul").before("<p class='notice'>Just updated</p>");
+  // $("li:contains('pine')").text("almonds");
+  // $("li").on("click", function () {
+  //   $(this).remove();
+  // });
+  // $("li").each(function () {
+  //   let ids = this.id;
+  //   $(this).append(`<em class="order">${ids}</em>`);
+  // });
 });
