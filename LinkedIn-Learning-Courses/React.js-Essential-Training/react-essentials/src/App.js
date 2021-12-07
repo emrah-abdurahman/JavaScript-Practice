f582b555304be018c0f4b1eeb07f3dac5d2163ff
Emrah Abdurahman <Emrah.Abdurahman@hotmail.com>
2021-12-07 19:59:12 +0000

    update src/App.js file to include React components and lists

--- LinkedIn-Learning-Courses/React.js-Essential-Training/react-essentials/src/App.js
+++ LinkedIn-Learning-Courses/React.js-Essential-Training/react-essentials/src/App.js
@@ -1,20 +1,45 @@
 import React from "react";
 import "./App.css";
 
-function Header() {
+function Header(props) {
   return (
     <header>
-      <h1>Eve's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   );
 }
 
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around.</p>
+      <ul style={{ textAlign: "left" }}>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+const dishes = ["Macaroni and Cheese", "Salmon", "Tofu with Vegetables"];
+
+const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <h2>Main</h2>
-      <h3>Footer</h3>
+      <Header name="Emrah" />
+      <Main adjective="amazing" dishes={dishObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
