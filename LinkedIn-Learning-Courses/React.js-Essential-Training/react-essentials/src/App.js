91270f0507a5e083957eeb512758ecace1a1eeef
Emrah Abdurahman <Emrah.Abdurahman@hotmail.com>
2021-12-08 22:44:13 +0000

    continue course

--- LinkedIn-Learning-Courses/React.js-Essential-Training/react-essentials/src/App.js
+++ LinkedIn-Learning-Courses/React.js-Essential-Training/react-essentials/src/App.js
@@ -1,47 +1,16 @@
 import React from "react";
 import "./App.css";
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
+function SecretComponent() {
+  return <h1>Secret information for authorized users only</h1>;
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
+function RegularComponent() {
+  return <h1>Everyone can see this component.</h1>;
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = ["Macaroni and Cheese", "Salmon", "Tofu with Vegetables"];
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Emrah" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App({ authorized }) {
+  return <>{authorized ? <SecretComponent /> : <RegularComponent />}</>;
 }
 
 export default App;
